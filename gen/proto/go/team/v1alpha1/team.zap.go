a18d81f1d704040632a87b9ddc11c8f73d393c26
Joshua Wilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2022-02-04 09:15:03 +0800

    add request status enum

--- gen/proto/go/team/v1alpha1/team.zap.go
+++ gen/proto/go/team/v1alpha1/team.zap.go
@@ -7,11 +7,11 @@ import (
 	fmt "fmt"
 	math "math"
 	proto "github.com/golang/protobuf/proto"
+	_ "github.com/common-fate/cf-protos/gen/proto/go/certrail/v1alpha1"
 	_ "google.golang.org/protobuf/types/known/timestamppb"
 	_ "google.golang.org/protobuf/types/known/durationpb"
 	_ "github.com/envoyproxy/protoc-gen-validate/validate"
 	_ "github.com/common-fate/gconfig/gen/gconfig/v1alpha1"
-	_ "github.com/common-fate/cf-protos/gen/proto/go/certrail/v1alpha1"
 	go_uber_org_zap_zapcore "go.uber.org/zap/zapcore"
 	github_com_golang_protobuf_ptypes "github.com/golang/protobuf/ptypes"
 )
@@ -80,7 +80,7 @@ func (m *RoleAccessRequest) MarshalLogObject(enc go_uber_org_zap_zapcore.ObjectE
 	}
 
 	keyName = "status" // field status = 2
-	enc.AddString(keyName, m.Status)
+	enc.AddString(keyName, m.Status.String())
 
 	keyName = "timestamp" // field timestamp = 3
 	if t, err := github_com_golang_protobuf_ptypes.Timestamp(m.Timestamp); err == nil {
