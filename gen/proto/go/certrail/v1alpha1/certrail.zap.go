7da802a9f02979ae0eac87d044a5430431fd1104
Joshua Wilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2022-02-11 16:03:11 +0800

    generate

--- gen/proto/go/certrail/v1alpha1/certrail.zap.go
+++ gen/proto/go/certrail/v1alpha1/certrail.zap.go
@@ -7,15 +7,9 @@ import (
 	fmt "fmt"
 	math "math"
 	proto "github.com/golang/protobuf/proto"
-<<<<<<< HEAD
-	_ "github.com/envoyproxy/protoc-gen-validate/validate"
-	_ "google.golang.org/protobuf/types/known/durationpb"
-	_ "google.golang.org/protobuf/types/known/timestamppb"
-=======
 	_ "google.golang.org/protobuf/types/known/timestamppb"
 	_ "github.com/envoyproxy/protoc-gen-validate/validate"
 	_ "google.golang.org/protobuf/types/known/durationpb"
->>>>>>> main
 	go_uber_org_zap_zapcore "go.uber.org/zap/zapcore"
 	github_com_golang_protobuf_ptypes "github.com/golang/protobuf/ptypes"
 )
